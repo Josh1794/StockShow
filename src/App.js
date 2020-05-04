d802abcb145a9c87e905cf71c6fbdb01047e0765
Joshua Grossman <Josh1794@gmail.com>
2020-05-04 16:25:52 -0400

    black page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
-  return <div> Hello world</div>;
+  return (
+    <div className='App'>
+      Stock Show Coming Soon <img src={logo} className='App-logo' alt='logo' />
+    </div>
+  );
 }
 
 export default App;
