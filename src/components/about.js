97c803dc482f4c56d75f5ceff485dcb5e9f5f120
Joshua Grossman <Josh1794@gmail.com>
2020-05-10 15:40:34 -0400

    testing

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,15 @@ import React from 'react';
  */
 export default function About() {
   return (
-    <div className='about'>
+    <div
+      className='about'
+      styles={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
       <h3>About Stock Show</h3>
       <div>ADD ABOUT SECTION HERE</div>
     </div>
