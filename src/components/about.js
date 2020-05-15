a59ecb1bdd1098a613a9e263e0b48026a5e60240
Joshua Grossman <Josh1794@gmail.com>
2020-05-15 17:34:21 -0400

    deleted firebase

--- src/components/about.js
+++ src/components/about.js
@@ -3,8 +3,18 @@ import React from 'react';
 export default function About() {
   return (
     <div className='mainPage'>
-      <h3>About Stock Show</h3>
-      <div>ADD ABOUT SECTION HERE</div>
+      <h2>About Stock Show</h2>
+      <div>
+        <h3>A free simulated stock protfolio app using realtime data</h3>
+        <div>
+          <h4>Features</h4>
+          <ul>
+            <li>Create a portfolio(s)</li>
+            <li>Research and Add Stocks to one of your portfolios</li>
+            <li>Track performance</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
